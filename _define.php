d4d19113e399315f0f8a328aee025199e8304ca2
Jean-Christian Denis <contact@jcdenis.fr>
2021-08-26 01:37:09 +0200

    cleanup define file (psr2 and short array and link)

--- _define.php
+++ _define.php
@@ -9,5 +9,22 @@
 # A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # -- END LICENSE BLOCK ------------------------------------
-if (!defined('DC_RC_PATH')){return;}
-$this->registerModule(	/* Name */			"kUtRL",	/* Description*/		"Use, create and serve short url on your blog",	/* Author */			"JC Denis",	/* Version */			'2011.03.24',	/* Permissions */		'admin');?>
+
+if (!defined('DC_RC_PATH')) {
+    return null;
+}
+
+$this->registerModule(
+    'kUtRL',
+    'Use, create and serve short url on your blog',
+    'Jean-Christian Denis and contributors',
+    '2011.03.24',
+    [
+        'permissions' => 'admin',
+        'type' => 'plugin',
+        'dc_min' => '2.19',
+        'support' => 'https://github.com/JcDenis/kUtRL',
+        'details' => 'http://plugins.dotaddict.org/dc2/details/kUtRL',
+        'repository' => 'https://raw.githubusercontent.com/JcDenis/kUtRL/master/repository.xml'
+    ]
+);
