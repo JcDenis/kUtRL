922111a0cb8316ad0d04e32dd73709007a5cdbe1
Jean-Christian Denis <contact@jcdenis.fr>
2025-09-13 16:12:23 +0200

    release 2025.09.13

--- _define.php
+++ _define.php
@@ -17,7 +17,7 @@ $this->registerModule(
     'Links shortener',
     'Use, create and serve short url on your blog',
     'Jean-Christian Denis and contributors',
-    '2025.09.11',
+    '2025.09.13',
     [
         'requires'    => [['core', '2.36']],
         'permissions' => 'My',
